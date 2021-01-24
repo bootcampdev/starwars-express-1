49a67773ff5e946148afa9e955723990e5f8ab77
kihBootCamp <kimberleyheuer@hotmail.com>
2021-01-24 15:14:04 -0600

    init

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+
+// for running on a server use the current assigned port or assisgn a default
 const PORT = process.env.PORT || 3000
+
+//
+// middleware: will need for posting - required for all express for any request comming in
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -36,6 +41,9 @@ const characters = [
 /**
  * HTML ROUTES
  */
+// request and response
+// home route 
+
 app.get('/', (req, res) => {
   res.sendFile( path.join(__dirname + '/public/index.html'))
 })
@@ -48,6 +56,9 @@ app.get('/add', (req, res) => {
  * API ROUTES
  */
 
+// route to see /api/characters
+// the api is convention to indicate that data is being returned
+
 // /api/characters - show all character data
 app.get('/api/characters', (req, res) => {
   res.json(characters)
@@ -55,20 +66,38 @@ app.get('/api/characters', (req, res) => {
 
 // /api/characters/:routeName
 app.get('/api/characters/:routeName', (req, res) => {
+  
+  console.log(req.params.routeName);
   const targetCharacter = req.params.routeName
+  console.log(targetCharacter);
+
   const character = characters.find(character => {
     return character.routeName === targetCharacter
   })
+  console.log(character);
   res.json(character)
 })
 
 // add new characters
+//
+// adding a new character, it is being sent via the body (using postman)
+
 app.post('/api/characters/add', (req, res) => {
   const newCharacter = req.body
+
+  // make new route name based on the name, remove spaces and lower case
+
   newCharacter.routeName = newCharacter.name.replace(/ /g, '').toLowerCase()
+
   characters.push(newCharacter)
+
+  //
+  // send a status back instead of just end
   res.status(200).send()
 })
+
+//
+// start server so it starts and listens
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
